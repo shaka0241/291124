bb8d11311d2b02eecb8c932fc357ec0e4e7fb17e
Alberto Rojas <shaka0241@gmail.com>
2024-11-29 23:33:39 +0100

    add components

---
+++ src/components/Education.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container mt-5">
+        <h2 class="mb-4">Educación</h2>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="(edu,index) in education" :key="index">
+                <h5>{{ edu.institute }}</h5>
+                <p>{{ edu.degree }} | {{ edu.period }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Education',
+    data() {
+        return {
+            education: [
+                {
+                    institute: 'Instituto Universitario de Tecnología READIC (UNIR)',
+                    degree: 'TSU en Informática',
+                    period: '2008 - 2012'
+                },
+                {
+                    institute: 'Universidad Nacional Experimental “Rafael María Baralt”',
+                    degree: 'Licenciado en Educación, mención Matemática y Física',
+                    period: '2006 - 2011'
+                }
+            ],
+        };
+    }
+};
+</script>
+
+<style scoped>
+.list-group-item {
+    margin-bottom: 1rem;
+}
+</style>
---
+++ src/components/Presentation.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container text-center mt-5">
+        <h1>{{ name }}</h1>
+        <p class="lead">{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Presentation',
+    data() {
+        return {
+            name: 'Alberto Rojas - Desarrollador Front-End',
+            description: 'Desarrollador Front-End con experiencia en Javascript, VueJS y ReactJS'
+        };
+    }
+};
+</script>
+
+<style scoped>
+.container {
+    padding: 2rem;
+}
+</style>
